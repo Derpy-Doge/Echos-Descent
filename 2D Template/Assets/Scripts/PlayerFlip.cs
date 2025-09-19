efba1b5c8671c8fde6da4ac8a34b8ba5325de78d
MUKILTEO\1725577 <kbw2205@icloud.com>
2025-09-19 12:16:16 -0700

    fixing sprite flip

--- 2D Template/Assets/Scripts/PlayerFlip.cs
+++ 2D Template/Assets/Scripts/PlayerFlip.cs
@@ -1,25 +1,48 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerFlip : MonoBehaviour
 {
+    [SerializeField] 
+    private SpriteRenderer spriteRenderer;
+
     private float horizontalInput;
-    private bool faceingLeft = true;
+    private bool facingLeft = true;
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        SetupDirectionByScale();
+       
+        SetupDirectionByComponent();
     }
 
     private void SetupDirectionByScale()
     {
-        if(horizontalInput < 0 && faceingLeft || horizontalInput > 0 && !faceingLeft)
+        if(horizontalInput < 0 && facingLeft || horizontalInput > 0 && !facingLeft)
         {
-            faceingLeft = !faceingLeft;
+            facingLeft = !facingLeft;
             Vector3 playerScale = transform.localScale;
             playerScale.x *= -1;
             transform.localScale = playerScale;
         }
+    }
+
+    private void SetupDirectionByComponent()
+    {
+        if(horizontalInput < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(horizontalInput > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+    }
+
+    private void SetupDirectionByRotation()
+    {
+        if (horizontalInput < 0 && facingLeft || horizontalInput > 0 && !facingLeft) ; 
     }
 }
